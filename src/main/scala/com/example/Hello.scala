28acd2a3713f02b0a4fc03ed158365ee82ace914
shigemk2 <i.am.shige@gmail.com>
2016-03-26 16:04:20 +0900

    Add playing video demo

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,15 @@
 package com.example
 
+import com.xuggle.mediatool.{IMediaReader, IMediaViewer, ToolFactory}
+
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val reader: IMediaReader = ToolFactory.makeReader("assets/severestormovermandurahmp4.mp4")
+    reader.addListener(ToolFactory.makeViewer(IMediaViewer.Mode.FAST_VIDEO_ONLY))
+
+    while (reader.readPacket() == null) {
+      println(reader.getUrl)
+    }
+    println("end")
   }
 }
